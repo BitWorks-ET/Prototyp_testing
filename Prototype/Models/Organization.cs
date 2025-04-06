b72b72bf6943524648c9e5062243ab3c056ab927
john04022 <i23018@hb.dhbw-stuttgart.de>
2025-04-06 15:25:24 +0200

    Add files via upload

---
+++ Prototype/Models/Organization.cs
@@ -0,0 +1,15 @@
+﻿namespace Prototype.Models;
+
+public class Organization
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Domain { get; set; }
+    
+    public List<Event> Events { get; set; }
+
+    public List<Person> OrgAdmins { get; set; }
+    public List<Person> OrgMembers { get; set; }
+}
---
+++ Prototype/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Prototype.Models;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime? Birthday { get; set; }
+    public Gender? Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public string JobTitle { get; set; }
+
+    public int Id { get; set; }
+    public string eMail { get; set; }
+    public string PasswordHash { get; set; }
+}
